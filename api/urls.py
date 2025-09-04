907869854d492472bd1f1797ac09b63427487608
Giovanni MARC <gvnnmarc@gmail.com>
2025-09-03 22:12:57 -0400

    update tablesaloons type view

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from api.views import (
-    TableSaloonViewSet, ReservationViewSet,
+    ReservationViewSet,
     NotificationViewSet, ReportViewSet, ScheduleViewSet,
-    RegisterView, UserDetailView, UserListView, AdminUpdateRoleView, MenuListCreateView, MenuDetailView
+    RegisterView, UserDetailView, UserListView, AdminUpdateRoleView, MenuListCreateView, MenuDetailView, table_saloons_list, table_saloon_detail
 )
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -18,14 +18,17 @@ urlpatterns = [
     path('users/', UserListView.as_view(), name='user_list'),
     path('users/<int:pk>/role/', AdminUpdateRoleView.as_view(), name='admin_update_role'),
     
-    # Menu 
+    # Menus 
     path('menus/', MenuListCreateView.as_view(), name='menu-list-create'),
     path('menus/<int:pk>/', MenuDetailView.as_view(), name='menu-detail'),
+    
+    # TableSaloons
+    path('tablesaloons/', table_saloons_list, name='tablesaloons-list-create'),
+    path('tablesaloons/<int:pk>/', table_saloon_detail, name='tablesaloons-detail')
 ]
 
 # to be continue
 router = DefaultRouter()
-router.register('tablesaloons', TableSaloonViewSet)
 router.register('reservations', ReservationViewSet)
 router.register('notifications', NotificationViewSet)
 router.register('reports', ReportViewSet)
